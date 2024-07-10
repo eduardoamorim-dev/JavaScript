c401fedfe6b42b6d395d69b673cb9f30057fd05b
caduamorimm-dev <amorimm.dev@gmail.com>
2024-07-09 22:57:03 -0300

    feat: alert, confirm and prompt

---
+++ 6 - alert, confirm e prompt(Navegador)/js/script.js
@@ -0,0 +1,13 @@
+// alert("Mensagem que vai aparecer no alerta");
+// confirm("Deseja realmente apagar?");
+// prompt("Digite o seu nome");
+
+// Exercicio
+
+let numero1 = prompt("Digite um numero.");
+let numero2 = prompt("Digite outro numero numero.");
+
+numero1 = parseInt(numero1);
+numero2 = parseInt(numero2);
+
+alert(`O resultado da soma foi:  ${numero1 + numero2}`);
